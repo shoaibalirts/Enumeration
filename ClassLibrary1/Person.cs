b82abcea06ac6c9525d859c351eb40d771d88ecc
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-10 18:50:10 +0200

    Add project files.

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,15 @@
+﻿public class Person
+{
+    public string PersonName { set; get; }
+    public string Email { set; get; }
+    public AgeGroupEnumeration AgeGroup { set; get; }
+
+}
+
+public enum AgeGroupEnumeration
+{
+    Child,
+    Teenager,
+    Adult,
+    Senior
+}
---
+++ Enumeration/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.AgeGroup = AgeGroupEnumeration.Adult;
+        WriteLine(person.AgeGroup);
+    }
+}
